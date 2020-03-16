9d07eafd3135d337db0ea2a1f4c3bbe592c25ba8
Doman Gaul <domaneq@gmail.com>
2020-03-16 11:51:48 +0100

    initial commit

---
+++ src/pages/Home/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Button from '../../components/Button/index';
+import './style.scss'
+
+const Home = () => {
+    return (
+        <>
+        <h1>Home</h1>
+        <Button />
+        </>
+    )
+}
+
+export default Home;
